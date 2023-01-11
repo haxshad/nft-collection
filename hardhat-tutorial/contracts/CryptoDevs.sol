4fae9c6ff7a5aa13745da4c9d6d28671871a7ca4
Harshad Dewangan <100013084+haxshad@users.noreply.github.com>
2023-01-11 22:08:32 +0530

    needless

--- hardhat-tutorial/contracts/CryptoDevs.sol
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -54,8 +54,6 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
       */
     function startPresale() public onlyOwner {
         presaleStarted = true;
-        // Set presaleEnded time as current timestamp + 5 minutes
-        // Solidity has cool syntax for timestamps (seconds, minutes, hours, days, years)
         presaleEnded = block.timestamp + 5 minutes;
     }
 
